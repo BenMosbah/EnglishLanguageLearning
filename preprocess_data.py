49c8f966f980b44f38126e1842c71bb2f776a2d2
BenMosbah <benmosbahmohamed@gmail.com>
2022-11-14 21:59:29 +0000

    Initial commit

---
+++ preprocess_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy
+raw_data = pd.read_csv('Data/train.csv')
+preprocessed_data = raw_data.copy()
+preprocessed_data.loc[:, 'NbWords'] = preprocessed_data['full_text'].str.split(' ').str.len()
+preprocessed_data.loc[:, 'NbCharacters'] = preprocessed_data['full_text'].str.len()
+preprocessed_data.loc[:, 'NbSentences'] = preprocessed_data['full_text'].str.count("[.]")
+punctuation_regex = '[,]|[;]|[?]|[!]'
+preprocessed_data.loc[:, 'NbPunctutation'] = preprocessed_data['full_text'].str.count(punctuation_regex)
+preprocessed_data.loc[:, 'NbQuestions'] = preprocessed_data['full_text'].str.count('[?]')
+preprocessed_data.loc[:, 'NbExclamations'] = preprocessed_data['full_text'].str.count('[!]')
