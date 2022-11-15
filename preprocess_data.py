70ede0bb84773e3b13ba6e3923fe9dfcc02ea4b0
BenMosbah <benmosbahmohamed@gmail.com>
2022-11-15 19:44:53 +0000

    some more preprocessing. Tokenization of sentences and building tensors.

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
 import numpy
 raw_data = pd.read_csv('Data/train.csv')
 preprocessed_data = raw_data.copy()
@@ -10,3 +13,10 @@ punctuation_regex = '[,]|[;]|[?]|[!]'
 preprocessed_data.loc[:, 'NbPunctutation'] = preprocessed_data['full_text'].str.count(punctuation_regex)
 preprocessed_data.loc[:, 'NbQuestions'] = preprocessed_data['full_text'].str.count('[?]')
 preprocessed_data.loc[:, 'NbExclamations'] = preprocessed_data['full_text'].str.count('[!]')
+
+all_corpus = list(preprocessed_data['full_text'])
+tokenizer = Tokenizer(num_words=100, oov_token="<OOV>")
+tokenizer.fit_on_texts(all_corpus)
+sequences = tokenizer.texts_to_sequences(all_corpus)
+padded = pad_sequences(sequences)
+#padded is ready to be fed to a neural network. I think
